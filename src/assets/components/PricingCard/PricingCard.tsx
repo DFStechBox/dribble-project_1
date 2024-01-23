b9bf9b5ea62c1888e3f3d233b11c1cb46302c966
dfstech <100256992+DFStechBox@users.noreply.github.com>
2024-01-23 00:37:29 +0000

    Styled PricingCard comp

--- src/assets/components/PricingCard/PricingCard.tsx
+++ src/assets/components/PricingCard/PricingCard.tsx
@@ -10,13 +10,15 @@ interface Props {
 function PricingCard({ label, priceLabel, image, imageAlt }: Props) {
   return (
     <div className={styles.card}>
-      <div className={styles.card__wrapper}>
+      <div className={`${styles.card__wrapper} center-vertical`}>
         <span className={styles.card__label}>{label}</span>
         <div>
           <img className={styles.card__image} src={image} alt={imageAlt} />
         </div>
-        <span className={styles.card__priceLabel}>{priceLabel}</span>
-        <span>Every thing in Free, Plus</span>
+        <span className={styles["card__price-label"]}>{priceLabel}</span>
+        <span className={styles["card__benefits-label"]}>
+          Every thing in Free, Plus
+        </span>
         <ul className={styles.card__benefits}>
           <li className={styles.card__benefit}>Up to 10 users</li>
           <li className={styles.card__benefit}>Email Support, Call Support</li>
