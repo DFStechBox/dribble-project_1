3edd1796a502a3fb7623c972293f15d14fe11d7a
dfstech <100256992+DFStechBox@users.noreply.github.com>
2024-01-29 19:18:02 +0000

    Styling project. Having problems with typescript types. Restarting new project with React JS.

--- src/assets/components/Button/Button.tsx
+++ src/assets/components/Button/Button.tsx
@@ -1,5 +1,14 @@
-function Button() {
-  return <button>Click me</button>;
+import styles from "./Button.module.css";
+
+interface Props {
+  className: string;
+  children: React.ReactNode;
+}
+
+function Button({ className, children }: Props) {
+  return (
+    <button className={`${className} ${styles.button}`}>{children}</button>
+  );
 }
 
 export default Button;
--- src/assets/components/PricingCard/PricingCard.tsx
+++ src/assets/components/PricingCard/PricingCard.tsx
@@ -1,4 +1,5 @@
 import styles from "./PricingCard.module.css";
+import Button from "../Button/Button";
 
 interface Props {
   label: string;
@@ -6,11 +7,26 @@ interface Props {
   duration: string;
   image: string;
   imageAlt: string;
+  benefits: string[];
 }
 
-function PricingCard({ label, price, duration, image, imageAlt }: Props) {
+function PricingCard({
+  label,
+  price,
+  duration,
+  image,
+  imageAlt,
+  benefits,
+}: Props) {
+  const themeClasses = {
+    "Start-Up": styles["card--startup"],
+    Pro: styles["card--pro"],
+    Enterprise: styles["card--enterprise"],
+  };
+  const themeClass = themeClasses[label];
+
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${themeClass} center-vertical`}>
       <div className={`${styles.card__wrapper} center-vertical`}>
         <span className={styles.card__label}>{label}</span>
         <div>
@@ -24,34 +40,23 @@ function PricingCard({ label, price, duration, image, imageAlt }: Props) {
           Every thing in Free, Plus
         </span>
         <ul className={styles.card__benefits}>
-          <li className={`${styles.card__benefits} center-horizontal`}>
-            <img
-              alt="check"
-              className={styles["card__benefit-icon"]}
-              src="/icons/check.png"
-            />
-            Up to 10 users
-          </li>
-          <li className={`${styles.card__benefits} center-horizontal`}>
-            {" "}
-            <img
-              alt="check"
-              className={styles["card__benefit-icon"]}
-              src="/icons/check.png"
-            />
-            Email Support, Call Support
-          </li>
-          <li className={`${styles.card__benefits} center-horizontal`}>
-            {" "}
-            <img
-              alt="check"
-              className={styles["card__benefit-icon"]}
-              src="/icons/check.png"
-            />
-            1 Year Access
-          </li>
+          {benefits.map((benefit) => {
+            return (
+              <li
+                key={benefit}
+                className={`${styles.card__benefits} center-horizontal`}
+              >
+                <img
+                  alt="check"
+                  className={styles["card__benefit-icon"]}
+                  src="/icons/check.png"
+                />
+                {benefit}
+              </li>
+            );
+          })}
         </ul>
-        <button className={styles.card__button}>Choose</button>
+        <Button className={styles.card__button}>Cooose</Button>
       </div>
     </div>
   );
